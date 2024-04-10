9dc3cb30d224130f1ca39ac2749cf57430035bd9
MimHe <mimhe1381@gmail.com>
2024-04-10 20:01:15 +0330

    Add auth swagger for /auth/send-otp

---
+++ src/swagger/auth.swagger.js
@@ -0,0 +1,40 @@
+/**
+ * @swagger
+ * tags:
+ *  name: Auth
+ *  description: Auth Modules and Routes
+ */
+
+/**
+ * @swagger
+ *  components:
+ *      schemas:
+ *          SendOTP:
+ *              type: object
+ *              required:
+ *                  -   mobile
+ *              properties:
+ *                  mobile:
+ *                      type: string
+ */
+
+/**
+ * @swagger
+ *
+ * /auth/send-otp:
+ *  post:
+ *      summary: login/signup with OTP in this endpoint.
+ *      tags:
+ *          -   Auth
+ *      requestBody:
+ *          content:
+ *              application/x-www-form-urlencoded:
+ *                  schema:
+ *                      $ref: '#/components/schemas/SendOTP'
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/SendOTP'
+ *      responses:
+ *          200:
+ *              description: successful
+ */
