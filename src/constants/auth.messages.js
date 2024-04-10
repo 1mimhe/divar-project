5a27db9b842b89039be122fd67e34f01a91b8c16
MimHe <mimhe1381@gmail.com>
2024-04-10 20:29:16 +0330

    Add checkOTP to auth.service and Add related error messages

--- src/constants/auth.messages.js
+++ src/constants/auth.messages.js
@@ -1,5 +1,8 @@
 module.exports = {
     SendOTPSuccessfully: "OTP Sent Successfully.",
     NotFound: "User Not Found.",
-    OTPNotExpired: "OTP Code is not Expired. Please Try Later."
+    OTPNotExpired: "OTP Code is not Expired. Please Try Later.",
+    OTPExpired: "OTP Code is Expired. Please Get New Code.",
+    OTPIsIncorrect: "OTP Code is Wrong. Please Try Again.",
+    LoginSuccessfully: "User login successfully."
 }
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -4,7 +4,7 @@ const User = require("../models/user.model");
 const authMessages = require("../constants/auth.messages");
 
 async function sendOTP(mobile) {
-    const user = await User.find({mobile});
+    const user = await User.findOne({mobile});
     const now = new Date().getTime();
     const otp = {
         code: randomInt(10000, 99999),
@@ -15,7 +15,6 @@ async function sendOTP(mobile) {
         const newUser = await User.create({mobile, otp});
         return newUser;
     }
-
     if (user.otp && user.otp.expiresIn > now) {
         throw new createError.BadRequest(authMessages.OTPNotExpired);
     }
@@ -26,7 +25,21 @@ async function sendOTP(mobile) {
 }
 
 async function checkOTP(mobile, code) {
+    const user = await User.findByMobile(mobile);
+    const now = new Date().getTime();
 
+    if (user.otp?.expiresIn < now) {
+        throw new createError.Unauthorized(authMessages.OTPExpired);
+    }
+    if (user.otp?.code !== code) {
+        throw new createError.Unauthorized(authMessages.OTPIsIncorrect);
+    }
+
+    if (!user.verifiedMobile) {
+        user.verifiedMobile = true;
+        await user.save();
+    }
+    return user;
 }
 
 module.exports = {
