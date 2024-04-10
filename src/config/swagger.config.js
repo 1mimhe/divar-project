5b0d196f6a936c5d0b60e032f4b6f9af0894ad47
MimHe <mimhe1381@gmail.com>
2024-04-10 19:53:59 +0330

    Update swagger config

--- src/config/swagger.config.js
+++ src/config/swagger.config.js
@@ -4,13 +4,14 @@ const swaggerUI = require('swagger-ui-express');
 function SwaggerConfig(app) {
     const swaggerDocument = swaggerJsDoc({
         swaggerDefinition: {
+            openapi: "3.0.3",
             info: {
                 title: "divar-backend",
                 description: "A store project",
                 version: "1.0.0"
             }
         },
-        apis: []
+        apis: [process.cwd() + "/src/swagger/*"]
     });
 
     const swagger = swaggerUI.setup(swaggerDocument, {});
