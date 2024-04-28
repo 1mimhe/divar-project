9e72e198d82118ec7b1aad1b9efd976fd34de72a
MimHe <mimhe1381@gmail.com>
2024-04-28 10:49:26 +0330

    Update 'auth' middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,12 +5,12 @@ const User = require("../models/user.model");
 
 async function authorization(req, res, next) {
     try {
-        const token = req.cookie?.access_token;
+        const token = req.cookies?.access_token;
         if (!token) throw new createError.Unauthorized(authMessages.Unauthorized);
 
         const payload = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
         if (payload?.id) {
-            const user = await User.findById(payload.id, { otp: 0 });
+            const user = await User.findById(payload.id, { _id: 1, mobile: 1, createdAt: 1 });
             if (!user) throw new createError.Unauthorized(authMessages.UserNotFound);
             req.user = user;
             return next();
