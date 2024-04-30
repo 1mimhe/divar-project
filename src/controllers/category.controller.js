5c3144769f418470568195e37aac4b5138dce010
MimHe <mimhe1381@gmail.com>
2024-04-30 10:57:43 +0330

    Add 'Category' controller

---
+++ src/controllers/category.controller.js
@@ -0,0 +1,28 @@
+const categoryService = require("../services/category.service");
+const categoryMessages = require("../constants/category.messages");
+
+async function createCategory(req, res, next) {
+    try {
+        const { name, icon, parent } = req.body;
+        await categoryService.createCategory({ name, icon, parent });
+        return res.status(201).json({
+            message: categoryMessages.CategoryCreated
+        });
+    } catch (err) {
+        next(err);
+    }
+}
+
+async function findAllCategories(req, res, next) {
+    try {
+        const categories = await categoryService.findAllCategories();
+        return res.status(201).json(categories);
+    } catch (err) {
+        next(err);
+    }
+}
+
+module.exports = {
+    createCategory,
+    findAllCategories
+}
