575591a7f6d347e74d3b599b4a6640b701c14898
MimHe <mimhe1381@gmail.com>
2024-05-10 11:08:49 +0330

    Add 'Option' model and messages

---
+++ src/constants/option.messages.js
@@ -0,0 +1,5 @@
+module.exports = Object.freeze({
+    NoOptions: "No options were found for this category.",
+    AlreadyExists: "This option for this category already exists.",
+    Created: "New option created successfully."
+});
---
+++ src/models/option.model.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const createHttpError = require('http-errors');
+const OptionMessages = require("../constants/option.messages");
+
+const optionSchema = new mongoose.Schema({
+    title: {type: String, required: true},
+    key: {type: String, required: true},
+    type: {type: String, enum: ["number", "string", "boolean"], required: true},
+    enum: {type: Array, default: []},
+    guide: {type: String, required: false},
+    category: {type: mongoose.Types.ObjectId, ref: "Category", required: true}
+});
+
+optionSchema.statics.findOptions = async function(categoryId) {
+    const options = await this.find({category: categoryId});
+    if (!options.length) throw new createHttpError.NotFound(OptionMessages.NoOptions);
+    return options;
+}
+
+const OptionModel = mongoose.model("Option", optionSchema);
+module.exports = OptionModel;
